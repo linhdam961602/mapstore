6b671f92422ba82252fcd7efcc63ab7d6f1afead
Hieu Nguyen <hieunm.citynow@gmail.com>
2021-03-27 14:35:22 +0700

    Add background

--- src/components/BasicComponent/Divider/index.js
+++ src/components/BasicComponent/Divider/index.js
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import AntdDivider from 'antd/es/divider';
 import 'antd/es/divider/style/css';
+import classNames from 'classnames';
 
-const Divider = (props) => (
-  <AntdDivider {...props}>{props.children}</AntdDivider>
-);
+import styles from './styles.module.scss';
+
+const Divider = ({ className, ...props }) => {
+  const classes = classNames({
+    [styles.tino__divider]: true,
+    [className]: className || '',
+  });
+  return (
+    <AntdDivider {...props} className={classes}>
+      {props.children}
+    </AntdDivider>
+  );
+};
 
 export default Divider;
--- src/pages/Auth/LoginPage/index.jsx
+++ src/pages/Auth/LoginPage/index.jsx
@@ -36,116 +36,118 @@ const LoginPage = () => {
   }, [dispatch, form]);
 
   return (
-    <div className={styles.login__container}>
-      <Form
-        className={styles.login__form}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        form={form}
-      >
-        <h1 className={styles.login__title}>
-          {intl.formatMessage({
-            id: 'login.title',
-          })}
-        </h1>
-        <Form.Item
-          name={LOGIN_FORM_FIELDS.USERNAME}
-          rules={[
-            {
-              type: 'email',
-              message: 'The input is not valid E-mail!',
-            },
-            {
-              required: true,
-              message: 'Please input your E-mail!',
-            },
-          ]}
+    <div className={styles.login__background}>
+      <div className={styles.login__container}>
+        <Form
+          className={styles.login__form}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          form={form}
         >
-          <Input
-            placeholder={intl.formatMessage({
-              id: 'login.placeholders.email',
+          <h1 className={styles.login__title}>
+            {intl.formatMessage({
+              id: 'login.title',
             })}
-          />
-        </Form.Item>
-        <Form.Item
-          name={LOGIN_FORM_FIELDS.PASSWORD}
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Password!',
-            },
-          ]}
-        >
-          <Input
-            type="password"
-            placeholder={intl.formatMessage({
-              id: 'login.placeholders.password',
-            })}
-          />
-        </Form.Item>
-        <div className={styles['login__remember-me']}>
+          </h1>
           <Form.Item
-            name={LOGIN_FORM_FIELDS.REMEMBER}
-            valuePropName="checked"
-            noStyle
+            name={LOGIN_FORM_FIELDS.USERNAME}
+            rules={[
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ]}
           >
-            <Checkbox>
-              {intl.formatMessage({
-                id: 'login.text.rememberMe',
+            <Input
+              placeholder={intl.formatMessage({
+                id: 'login.placeholders.email',
               })}
-            </Checkbox>
+            />
           </Form.Item>
+          <Form.Item
+            name={LOGIN_FORM_FIELDS.PASSWORD}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              type="password"
+              placeholder={intl.formatMessage({
+                id: 'login.placeholders.password',
+              })}
+            />
+          </Form.Item>
+          <div className={styles['login__remember-me']}>
+            <Form.Item
+              name={LOGIN_FORM_FIELDS.REMEMBER}
+              valuePropName="checked"
+              noStyle
+            >
+              <Checkbox>
+                {intl.formatMessage({
+                  id: 'login.text.rememberMe',
+                })}
+              </Checkbox>
+            </Form.Item>
 
-          <a className="login-form-forgot" href="">
+            <a className="login-form-forgot" href="">
+              {intl.formatMessage({
+                id: 'login.text.forgotPassword',
+              })}
+            </a>
+          </div>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className={styles.login__button}
+              block
+            >
+              {intl.formatMessage({
+                id: 'login.buttons.login',
+              })}
+            </Button>
+          </Form.Item>
+          <div className={styles['login__create-account']}>
+            <span>
+              {intl.formatMessage({
+                id: 'login.text.notAMember',
+              })}
+            </span>
+            <a href={REGISTER_URL}>
+              {intl.formatMessage({
+                id: 'login.text.createAcc',
+              })}
+            </a>
+          </div>
+          <Divider plain>
             {intl.formatMessage({
-              id: 'login.text.forgotPassword',
+              id: 'login.text.useAcc',
             })}
-          </a>
-        </div>
-
-        <Form.Item>
+          </Divider>
           <Button
-            type="primary"
-            htmlType="submit"
-            className={styles.login__button}
+            className={styles['login__button-with-google']}
             block
+            icon={<GoogleOutlined style={{ color: '#ec5741' }} />}
           >
             {intl.formatMessage({
-              id: 'login.buttons.login',
+              id: 'login.buttons.loginWGoogle',
             })}
           </Button>
-        </Form.Item>
-        <div className={styles['login__create-account']}>
-          <span>
-            {intl.formatMessage({
-              id: 'login.text.notAMember',
-            })}
-          </span>
-          <a href={REGISTER_URL}>
-            {intl.formatMessage({
-              id: 'login.text.createAcc',
-            })}
-          </a>
-        </div>
-        <Divider plain>
-          {intl.formatMessage({
-            id: 'login.text.useAcc',
-          })}
-        </Divider>
-        <Button
-          className={styles['login__button-with-google']}
-          block
-          icon={<GoogleOutlined style={{ color: '#ec5741' }} />}
-        >
-          {intl.formatMessage({
-            id: 'login.buttons.loginWGoogle',
-          })}
-        </Button>
-      </Form>
+        </Form>
 
-      <div className={styles.login__background} />
+        <div className={styles.login__foreground} />
+      </div>
     </div>
   );
 };
