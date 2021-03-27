62e319844b0c95c4481aa3229f853f0218f3f27c
ThanhTruc <truc.le@metanet.co.kr>
2021-03-27 23:08:33 +0700

    mockup-data-sidebar-left

--- src/translations/vi/index.js
+++ src/translations/vi/index.js
@@ -1,9 +1,11 @@
 import * as common from './common';
 import * as auth from './auth';
+import * as menu from './menu';
 
 const combined = {
   ...common,
   ...auth,
+  ...menu,
 };
 
 export default combined;
