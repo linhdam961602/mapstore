05d29a84ea4139eb3252371c4a26d43d67493867
Hieu Nguyen <hieunm.citynow@gmail.com>
2021-04-03 00:26:11 +0700

    Remove unused

--- src/components/LayoutComponent/MainLayout/index.js
+++ src/components/LayoutComponent/MainLayout/index.js
@@ -1,37 +1,24 @@
-import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 
 import './styles.scss';
 
 import Layout from 'components/BasicComponent/Layout';
 import Footer from 'components/Footer';
 import Sider from 'containers/SidebarLeft';
-import Button from 'components/BasicComponent/Button';
-import { authActions } from 'pages/LoginPage/slices';
 
 const { Header, Content } = Layout;
 
-const MainLayout = (props) => {
-  const dispatch = useDispatch();
-
-  const onClick = useCallback(() => {
-    dispatch(authActions.logout());
-  }, [dispatch]);
-
-  return (
-    <Layout className="layout_main">
-      <Header className="header">
-        <Button onClick={onClick}>Logout</Button>
-      </Header>
-      <Layout className="site-layout">
-        <Sider />
-        <div className="content_wrapper">
-          <Content className="content">{props.children}</Content>
-        </div>
-      </Layout>
-      <Footer />
+const MainLayout = (props) => (
+  <Layout className="layout_main">
+    <Header className="header" />
+    <Layout className="site-layout">
+      <Sider />
+      <div className="content_wrapper">
+        <Content className="content">{props.children}</Content>
+      </div>
     </Layout>
-  );
-};
+    <Footer />
+  </Layout>
+);
 
 export default MainLayout;
